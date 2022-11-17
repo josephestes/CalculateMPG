f2f925bf8ab5ae48660b229e113080dc0c50f1be
Student <Student@DESKTOP-9LDA21V>
2022-11-17 15:01:16 -0500

    Add project files.

---
+++ CalculateMPG/Program.cs
@@ -0,0 +1,68 @@
+﻿/* 
+ * This program calculate MPG given miles driven and gallons used.
+ * 
+ * User inputs gallons used (e.g., 3) and miles driven (e.g., 60).
+ * 
+ * Program uses exception handling to check for numeric inputs and division by zero,
+ * and logic to check for unreasonable input (e.g., -1 or 1000).
+ */
+
+using System;
+using static System.Console;
+
+namespace CalculateMPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int milesDriven;
+            int gallonsUsed;
+            int milesPerGallon;
+
+            try
+            {
+                //Prompt user for miles driven
+                WriteLine("Please enter the number of miles driven: ");
+                milesDriven = Convert.ToInt32(ReadLine());
+
+                //Check for reasonable input
+                while (milesDriven < 0 || milesDriven > 1000)
+                {
+                    WriteLine("Invalid entry! Enter a positive value up to 1000: ");
+                    milesDriven = Convert.ToInt32(ReadLine());
+                }
+
+                //Prompt user for gallons used
+                WriteLine("Enter the number of gallons used: ");
+                gallonsUsed = Convert.ToInt32(ReadLine());
+
+                //Check for reasonable input
+                while (gallonsUsed < 0)
+                {
+                    WriteLine("Invalid entry! Enter a positive value: ");
+                    gallonsUsed = Convert.ToInt32(ReadLine());
+                }
+
+                //Calculate miles per gallon
+                milesPerGallon = milesDriven / gallonsUsed;
+
+            }
+            //Incorrect input type exception
+            catch (FormatException error)
+            {
+                milesPerGallon = 0;
+                WriteLine(error.Message);
+            }
+
+            //Dividing by zero exception
+            catch (DivideByZeroException error)
+            {
+                milesPerGallon = 0;
+                WriteLine(error.Message);
+            }
+
+            WriteLine("Your miles per gallon is: " + milesPerGallon);
+        }
+    }
+}
